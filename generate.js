b97392f42ff2b374b5db8fdb226cb2236bdde112
Jason <kimsj9484@gmail.com>
2021-09-12 17:20:10 +0900

    Implement viewer nav

--- generate.js
+++ generate.js
@@ -1,9 +1,22 @@
 const fs = require("fs");
 const path = require("path");
 
-const dir = fs.readdirSync(path.join(__dirname, "resources"));
+const resourcesPath = path.join(__dirname, "resources");
+const dirList = fs.readdirSync(resourcesPath);
+const readList = ["index.html", "style.css", "script.js"];
+const fileMap = {};
+
+for (const dir of dirList) {
+  for (const target of readList) {
+    const filePath = path.join(resourcesPath, dir, target);
+    fileMap[dir] ??= {};
+    const file = fs.readFileSync(filePath, { encoding: "utf-8" });
+    fileMap[dir][target.split(".")[1]] = file;
+  }
+}
 
 fs.writeFileSync(
   "resources.js",
-  `const resources = [${dir.map((x) => `"${x}"`).join(", ")}]`
+  `const resources = [${dirList.map((x) => `"${x}"`).join(", ")}];
+  const fileMap = ${JSON.stringify(fileMap)};`
 );
--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const getSelectedResource = () => {
+  const x = localStorage.getItem("selected-resource");
+  return resources.includes(x) ? x : null;
+};
+
 // Init resizer
 {
   const main = document.querySelector("#main");
@@ -57,19 +62,19 @@
     }
   });
 
-  const resizeWidth = localStorage.getItem("resize-width");
-  if (resizeWidth) nav.style.flexBasis = resizeWidth;
-  onWindowResize();
+  // Main
+  {
+    const resizeWidth = localStorage.getItem("resize-width");
+    if (resizeWidth) nav.style.flexBasis = resizeWidth;
+    onWindowResize();
+  }
 }
 
 // Init resource list
 {
   const ul = document.querySelector("#nav > ul");
   const iframe = document.querySelector("iframe");
-  const selectedResource = (function () {
-    const x = localStorage.getItem("selected-resource");
-    return resources.includes(x) ? x : null;
-  })();
+  const selectedResource = getSelectedResource();
   let activeListItem;
 
   const onResourceChange = (resource, li) => {
@@ -80,7 +85,7 @@
     activeListItem = li;
   };
 
-  resources.forEach((resource, idx) => {
+  resources.forEach((resource) => {
     const li = document.createElement("li");
     li.innerText = resource;
     li.addEventListener("click", () => onResourceChange(resource, li));
@@ -93,4 +98,63 @@
       ({ innerText }) => innerText === selectedResource
     ) || ul.firstChild
   );
+}
+
+// Viewer nav
+{
+  const resourceListEl = document.querySelector("#nav > ul");
+  const viewerNavEl = document.querySelector("#viewerNav");
+  const codeViewer = document.querySelector("#codeViewer");
+  const iframe = document.querySelector("iframe");
+  let selectedResource = getSelectedResource() || resources[0];
+  let selectedView;
+  let selectedViewLi;
+
+  const genFileList = (selectedResource) => {
+    // Viewer 외 메뉴 제외
+    while (viewerNavEl.children.length > 1) viewerNavEl.lastChild.remove();
+    const fileList = Object.keys(fileMap[selectedResource]);
+    for (const file of fileList) {
+      const li = document.createElement("li");
+      li.innerText = file.toUpperCase();
+      viewerNavEl.append(li);
+      li.addEventListener("click", function () {
+        selectView(this, file);
+      });
+    }
+  };
+
+  const selectView = (li, menu) => {
+    selectedViewLi?.classList.toggle("active");
+    li.classList.toggle("active");
+    selectedViewLi = li;
+    selectedView = menu;
+
+    if (selectedView !== "viewer") {
+      iframe.style.display = "none";
+      codeViewer.style.display = "block";
+      const content = fileMap[selectedResource][selectedView];
+      codeViewer.innerText = content;
+      codeViewer.scrollTo(0, 0);
+    } else {
+      iframe.style.display = "block";
+      codeViewer.style.display = "none";
+      iframe.scrollTo(0, 0);
+    }
+  };
+
+  viewerNavEl.firstElementChild.addEventListener("click", function () {
+    selectView(this, "viewer");
+  });
+
+  for (const item of resourceListEl.children) {
+    item.addEventListener("click", () => {
+      selectedResource = item.innerText;
+      genFileList(selectedResource);
+      selectView(viewerNavEl.firstElementChild, "viewer");
+    });
+  }
+
+  genFileList(selectedResource);
+  selectView(viewerNavEl.firstElementChild, "viewer");
 }
